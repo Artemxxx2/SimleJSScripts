c59785e7c556826749e6ebee33951037da10d989
Artemxxx2 <akolobnevstudio@gmail.com>
2021-09-24 20:38:12 +0300

    Lesson1

---
+++ indexJS1.js
@@ -0,0 +1,12 @@
+// variables initalazing
+let camelCase=0;
+let snake_case =1;
+let PascalCase=2;
+// log output
+console.log(camelCase);
+console.log(snake_case);
+console.log(PascalCase);
+// alert output
+alert(camelCase)
+alert(snake_case)
+alert(PascalCase)
