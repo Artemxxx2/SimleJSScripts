b8105a0f5b75d1c1b2ad121f3f4196a518a00b77
Artemxxx2 <akolobnevstudio@gmail.com>
2021-09-25 14:45:51 +0300

    Lesson11

---
+++ index11.js
@@ -0,0 +1,12 @@
+function name(arr1,arr2) {
+    let accum1 =arr1.reduce((prev,current)=>{
+        prev = prev*current
+         return prev
+        })
+    let accum2 =arr2.reduce((prev,current)=>{
+            prev = prev*current
+             return prev
+            })
+    return accum1 - accum2;
+}
+console.log(name([1,2,3],[1,2,7]));
